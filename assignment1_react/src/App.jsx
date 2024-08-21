fe9a7b81afed61b68bb71562fbbd76635091b5cd
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-21 21:12:05 +0500

    assignment1

--- assignment1_react/src/App.jsx
+++ assignment1_react/src/App.jsx
@@ -27,9 +27,20 @@ const products = [
 ];
 
 const products2 = [
-  { id: 1, name: 'Product 1', price: 100 },
-  { id: 2, name: 'Product 2', price: 150 },
-  { id: 3, name: 'Product 3', price: 200 },
+  {
+     id: 1,
+     name: 'Tapal Small',
+     price: 100
+   },
+  {
+     id: 2,
+     name: 'Tapal Medium',
+     price: 150
+     },
+    {
+    id: 3,
+    name: 'Tapal Large',
+    price: 200 },
   // Add more products as needed
 ];
 
@@ -58,13 +69,26 @@ const ProductList = () => {
          </table>
     
       {/* this is comment */}
-      {/* <ul>
-        {products2.map((products2) => (
-          <li key={products2.id}>
-            {products2.name} - ${products2.price}
-          </li>
+      
+      <h3>MILK</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Serial.No</th>
+              <th>Product Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+        {products2.map((data,ind) => (
+          <tr key={ind}>
+             <td>{ind + 1}</td>
+                <td>{data.name}</td>
+                <td>{data.price}</td>
+          </tr>
         ))}
-      </ul> */}
+        </tbody>
+         </table>
     </div>
   );
 };
