8e1de7b702041441e9cfe282e3d56f0d5aa8262f
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-21 20:04:06 +0500

    assignment1

---
+++ assignment1_react/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const products = [
+  { id: 1, name: 'Product 1', price: 100 },
+  { id: 2, name: 'Product 2', price: 150 },
+  { id: 3, name: 'Product 3', price: 200 },
+  // Add more products as needed
+];
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1>Online Store</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
+
