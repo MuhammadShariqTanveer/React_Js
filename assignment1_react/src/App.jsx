05c8e3c3cb9ef64698a8c2d566729dfd49f3290a
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-22 01:23:11 +0500

    assignment1

--- assignment1_react/src/App.jsx
+++ assignment1_react/src/App.jsx
@@ -29,20 +29,59 @@ const products = [
 const products2 = [
   {
      id: 1,
-     name: 'Tapal Small',
+     name: 'Milk Pack Small',
      price: 100
    },
   {
      id: 2,
-     name: 'Tapal Medium',
+     name: ' Milk Pack Medium',
      price: 150
      },
     {
     id: 3,
-    name: 'Tapal Large',
-    price: 200 },
+    name: 'Milk Pack Large',
+    price: 250 },
+    {
+      id: 3,
+      name: 'Tapal Medium',
+      price: 150 },
+      {
+        id: 3,
+        name: 'Tapal Large',
+        price: 250 },
+
+   
   // Add more products as needed
 ];
+
+const products3 = [
+  {
+     id: 1,
+     name: 'Pepsi 250ml',
+     price: 80
+   },
+  {
+     id: 2,
+     name: ' Pepsi 500ml',
+     price: 130
+     },
+    {
+    id: 3,
+    name: 'Pepsi 1L',
+    price: 170 },
+    {
+      id: 3,
+      name: 'Pepsi 1.5L',
+      price: 210 },
+      {
+        id: 3,
+        name: 'Pepsi 2.3L',
+        price: 260 },
+
+   
+  // Add more products as needed
+];
+
 
 
 const ProductList = () => {
@@ -50,9 +89,13 @@ const ProductList = () => {
     <div>
       <h1>ABC BAKERY</h1>
       <div className='flex'>
-        <h3>BISCUITS</h3>
         <table>
           <thead>
+            <tr className='empty'>
+              <td></td>
+              <td>BISCUITS</td>
+              <td></td>
+              </tr>
             <tr>
               <th>Serial.No</th>
               <th>Product Name</th>
@@ -72,9 +115,13 @@ const ProductList = () => {
     
       {/* this is comment */}
       
-      <h3>MILK</h3>
         <table>
           <thead>
+            <tr className='empty'>
+              <td></td>
+              <td>MILK</td>
+              <td></td>
+              </tr>
             <tr>
               <th>Serial.No</th>
               <th>Product Name</th>
@@ -87,10 +134,39 @@ const ProductList = () => {
              <td>{ind + 1}</td>
                 <td>{data.name}</td>
                 <td>{data.price}</td>
+             
           </tr>
         ))}
         </tbody>
          </table>
+
+ {/* this is comment */}
+      
+ <table>
+          <thead>
+            <tr className='empty'>
+              <td></td>
+              <td>COLD DRINKS</td>
+              <td></td>
+              </tr>
+            <tr>
+              <th>Serial.No</th>
+              <th>Product Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+        {products3.map((data,ind) => (
+          <tr key={ind}>
+             <td>{ind + 1}</td>
+                <td>{data.name}</td>
+                <td>{data.price}</td>
+             
+          </tr>
+        ))}
+        </tbody>
+         </table>
+
          </div>
     </div>
   );
