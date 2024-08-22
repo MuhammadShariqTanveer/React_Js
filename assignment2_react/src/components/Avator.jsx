78a785744ebcf598dcfaf1e0ac7f0c0b5ad97698
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-23 00:46:13 +0500

    assignment

--- assignment2_react/src/components/Avator.jsx
+++ assignment2_react/src/components/Avator.jsx
@@ -1,10 +1,10 @@
 function Avator(){
     return(
-        <div className="flex">
+        <div className="">
             <div>
             <img className="h-20 w-20 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
             </div>
-            <div>
+            <div className="flex flex-col ">
                 <h2>Shariq Raja</h2>
                 <h2>Front-End Developer</h2>
             </div>
