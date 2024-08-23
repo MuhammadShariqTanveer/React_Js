629c36590c253d4d14fcfd1f393eaf7ee7610253
Muhammad Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-24 01:55:18 +0500

    assignment

--- assignment2_react/src/components/Avator.jsx
+++ assignment2_react/src/components/Avator.jsx
@@ -1,10 +1,10 @@
- import React from "react";
+import React from "react";
 
- function Avator({img,onClick}) {
+function Avator({ img, onClick }) {
   return (
     <div className="my-6">
-      <img className="w-32 h-32 cursor-pointer " src={img} alt="Avatar" onClick={onClick}  />
-      </div>
+      <img className="w-32 h-32 cursor-pointer " src={img} alt="Avatar" onClick={onClick} />
+    </div>
   );
 }
 
