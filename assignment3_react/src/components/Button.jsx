9a15fbc5e090dd4b8cdf71ec1d99a13ed7d52eb3
Shariq Tanveer <136991351+MuhammadShariqTanveer@users.noreply.github.com>
2024-08-26 18:41:44 +0500

    Delete assignment3_react directory

--- assignment3_react/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({text,onClick}) {
-    return (
-      <button onClick={onClick} className="p-1 px-4 m-1 rounded bg-orange-400 hover:bg-red-100">
-        {text}
-      </button>
-    );
-  }
-  
-  export default Button
